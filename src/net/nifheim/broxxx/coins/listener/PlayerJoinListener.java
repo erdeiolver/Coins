838eb03c94bd8506725dd91a7c6513b10c155a0b
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-05-05 21:34:36 -0300

    Cleanup

--- src/net/nifheim/broxxx/coins/listener/PlayerJoinListener.java
+++ src/net/nifheim/broxxx/coins/listener/PlayerJoinListener.java
@@ -1,9 +1,6 @@
 package net.nifheim.broxxx.coins.listener;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,7 +9,6 @@ import net.nifheim.broxxx.coins.databasehandler.MySQL;
 import org.bukkit.Bukkit;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -20,52 +16,18 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoinListener implements Listener {
 
     private final FileConfiguration config = Main.getInstance().getConfig();
-    private final String prefix = config.getString("MySQL.Prefix");
+    private static final MySQL mysql = new MySQL();
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Bukkit.getServer().getScheduler().runTaskLaterAsynchronously(Main.getInstance(), () -> {
             if (config.getBoolean("MySQL.Use")) {
                 try {
-                    Player p = e.getPlayer();
-                    String name;
-                    if (config.getBoolean("Online Mode")) {
-                        name = p.getUniqueId().toString();
-                    } else {
-                        name = p.getName();
-                    }
-
-                    Connection c = MySQL.getConnection();
-                    Statement check = c.createStatement();
-                    if (config.getBoolean("Online Mode")) {
-                        ResultSet res = check.executeQuery("SELECT uuid FROM " + prefix + "Data WHERE uuid = '" + name + "';");
-                        if (!res.next()) {
-                            Statement update = c.createStatement();
-                            update.executeUpdate("INSERT INTO " + prefix + "Data VALUES ('" + name + "', '" + p.getName() + "', 0.0, " + System.currentTimeMillis() + ");");
-                        } else {
-                            Statement update = c.createStatement();
-                            update.executeUpdate("UPDATE " + prefix + "Data SET nick = " + p.getName() + " WHERE uuid = '" + name + "';");
-                        }
-                    } else {
-                        ResultSet res = check.executeQuery("SELECT nick FROM " + prefix + "Data WHERE nick = '" + name + "';");
-                        if (!res.next()) {
-                            Statement update = c.createStatement();
-                            update.executeUpdate("INSERT INTO " + prefix + "Data VALUES ('" + p.getUniqueId() + "', '" + name + "', 0.0, " + System.currentTimeMillis() + ");");
-                        } else {
-                            Statement update = c.createStatement();
-                            update.executeUpdate("UPDATE " + prefix + "Data SET uuid = " + p.getUniqueId() + " WHERE nick = '" + name + "';");
-                        }
-                    }
-
+                    mysql.createPlayer(e.getPlayer());
                 } catch (SQLException ex) {
-                    if (ex.getSQLState().equals("closed")) {
-                        Logger.getLogger(PlayerJoinListener.class.getName()).log(Level.WARNING, "Seems that the database connection is closed.");
-                    } else {
-                        Logger.getLogger(PlayerJoinListener.class.getName()).log(Level.WARNING, "Something was wrong executing this query, the error code is: " + ex.getErrorCode(), ex.getCause());
-                    }
+                    Logger.getLogger(PlayerJoinListener.class.getName()).log(Level.WARNING, "Unable to create an entry in the database for player: " + e.getPlayer().getName() + " the error code is: " + ex.getErrorCode(), ex);
                 }
-            }
-            else {
+            } else {
                 Main.ff.createPlayer(e.getPlayer());
             }
         }, 5L);
