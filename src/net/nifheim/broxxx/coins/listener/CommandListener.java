ccede54c48e0c560e72300dd0683d7f7b5ac57d5
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-05-12 10:50:49 -0300

    Bug fix

--- src/net/nifheim/broxxx/coins/listener/CommandListener.java
+++ src/net/nifheim/broxxx/coins/listener/CommandListener.java
@@ -1,7 +1,5 @@
 package net.nifheim.broxxx.coins.listener;
 
-import java.sql.SQLException;
-
 import net.nifheim.broxxx.coins.CoinsAPI;
 import net.nifheim.broxxx.coins.Main;
 
@@ -12,19 +10,18 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CommandListener implements Listener {
 
-    //    private Main plugin;
     private final FileConfiguration config = Main.getInstance().getConfig();
     private Main plugin;
 
     @EventHandler
-    public void onCommandEvent(PlayerCommandPreprocessEvent e) throws SQLException {
+    public void onCommandEvent(PlayerCommandPreprocessEvent e) {
         String msg = e.getMessage().toLowerCase();
-        if (config.getInt("Command Cost." + msg) != 0) {
-            if (CoinsAPI.getCoins(e.getPlayer()) < config.getInt("Command Cost." + msg)) {
+        if (config.getDouble("Command Cost." + msg) != 0) {
+            if (CoinsAPI.getCoins(e.getPlayer()) < config.getDouble("Command Cost." + msg)) {
                 e.setCancelled(true);
                 e.getPlayer().sendMessage(plugin.rep(config.getString("Messages.Errors.No Coins")));
             } else {
-                CoinsAPI.takeCoins(e.getPlayer(), config.getInt("Command Cost." + msg));
+                CoinsAPI.takeCoins(e.getPlayer(), config.getDouble("Command Cost." + msg));
             }
         }
     }
